a6f6978c6c22ede2c0c0c5e30a1d202c04c4721f
Axel Halintar <axeldevara@gmail.com>
2019-10-22 20:31:15 +1100

    created SPA

---
+++ Lonsdale-SPA/src/app/app.component.spec.ts
@@ -0,0 +1,31 @@
+import { TestBed, async } from '@angular/core/testing';
+import { AppComponent } from './app.component';
+
+describe('AppComponent', () => {
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [
+        AppComponent
+      ],
+    }).compileComponents();
+  }));
+
+  it('should create the app', () => {
+    const fixture = TestBed.createComponent(AppComponent);
+    const app = fixture.debugElement.componentInstance;
+    expect(app).toBeTruthy();
+  });
+
+  it(`should have as title 'Lonsdale-SPA'`, () => {
+    const fixture = TestBed.createComponent(AppComponent);
+    const app = fixture.debugElement.componentInstance;
+    expect(app.title).toEqual('Lonsdale-SPA');
+  });
+
+  it('should render title', () => {
+    const fixture = TestBed.createComponent(AppComponent);
+    fixture.detectChanges();
+    const compiled = fixture.debugElement.nativeElement;
+    expect(compiled.querySelector('.content span').textContent).toContain('Lonsdale-SPA app is running!');
+  });
+});
---
+++ Lonsdale.API/Controllers/ValueController.cs
@@ -0,0 +1,28 @@
+using System.Linq;
+using System.Threading.Tasks;
+using Lonsdale.API.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace Lonsdale.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ValueController : ControllerBase
+    {
+        private readonly DataContext _context;
+        public ValueController(DataContext context){
+            _context = context;
+        }
+        [HttpGet]
+        public async Task<IActionResult> Get(){
+            var values = await _context.Values.ToListAsync();
+            return Ok(values);
+        }
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetValue(int id){
+            var values = await _context.Values.FirstOrDefaultAsync(x => x.Id == id);
+            return Ok(values);
+        }
+    }
+}
