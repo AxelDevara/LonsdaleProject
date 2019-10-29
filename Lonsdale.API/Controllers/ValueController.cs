f64cc1a615ebfd468a07a2cfa7db605ac8ca1a57
Axel Halintar <axeldevara@gmail.com>
2019-10-29 17:21:25 +1100

    Token system and authorization

--- Lonsdale.API/Controllers/ValueController.cs
+++ Lonsdale.API/Controllers/ValueController.cs
@@ -1,11 +1,13 @@
 using System.Linq;
 using System.Threading.Tasks;
 using Lonsdale.API.Data;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 
 namespace Lonsdale.API.Controllers
 {
+    [Authorize]
     [Route("api/[controller]")]
     [ApiController]
     public class ValueController : ControllerBase
